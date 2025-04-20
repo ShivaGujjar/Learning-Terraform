9d3fb6ae1022c6c5541229c8276943800708f111
ShivaGujjar <sgujjar@gmail.com>
2025-04-21 00:05:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
-provider "aws" {
-  region = "us-west-2"
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
